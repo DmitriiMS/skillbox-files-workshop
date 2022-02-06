c082a6f37b4d440ff41bf47c85b02abe93e2f3c3
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-02-06 15:15:54 +0300

    added size conversion methods

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPath = "/home/dms/";
+        String folderPath = "/tmp/";
         File folder = new File(folderPath);
 
 
@@ -11,11 +11,11 @@ public class Main {
         FolderSizeCalculator calculator = new FolderSizeCalculator(folder);
         ForkJoinPool pool = new ForkJoinPool();
         long size = pool.invoke(calculator);
-        System.out.println(size);
+        System.out.println(getHumanReadableSize(size));
         System.out.println(System.nanoTime() - start + " ns");
         System.out.println("\n--------------------\n");
         start = System.nanoTime();
-        System.out.println(getFolderSize(folder));
+        System.out.println(getHumanReadableSize(getFolderSize(folder)));
         System.out.println(System.nanoTime() - start + " ns");
     }
 
@@ -31,5 +31,33 @@ public class Main {
             }
         }
         return sum;
+    }
+
+    public static String getHumanReadableSize(long size) {
+        final long TB = 1099511627776L;
+        final long GB = 1073741824L;
+        final long MB = 1048576L;
+        final long KB = 1024L;
+        if(size >= TB) { return (int) (Math.round((double) size / TB)) + "T"; }
+        if(size >= GB) { return (int) (Math.round((double) size / GB)) + "G"; }
+        if(size >= MB) { return (int) (Math.round((double) size / MB)) + "M"; }
+        if(size >= KB) { return (int) (Math.round((double) size / KB)) + "K"; }
+        return size + "B";
+    }
+
+    public static long getSizeFromHumanReadable (String size) {
+        long digits = Long.parseLong(size.substring(0, size.length() - 1));
+        switch (size.toCharArray()[size.length() - 1]) {
+            case 'K':
+                return digits * 1024;
+            case 'M':
+                return digits * 1024 * 1024;
+            case 'G':
+                return digits * 1024 * 1024 * 1024;
+            case 'T':
+                return digits * 1024 * 1024 * 1024 * 1024;
+            default:
+                return digits;
+        }
     }
 }
