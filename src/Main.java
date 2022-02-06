4d215ebb8fb8a44e9c4bbe1e477f3331e5ad5115
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-02-06 14:31:26 +0300

    added forkjoin variant

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
         String folderPath = "/home/dms/";
         File folder = new File(folderPath);
 
+
+        long start = System.nanoTime();
+        FolderSizeCalculator calculator = new FolderSizeCalculator(folder);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+        System.out.println(System.nanoTime() - start + " ns");
+        System.out.println("\n--------------------\n");
+        start = System.nanoTime();
         System.out.println(getFolderSize(folder));
+        System.out.println(System.nanoTime() - start + " ns");
     }
 
     public static long getFolderSize(File folder){
